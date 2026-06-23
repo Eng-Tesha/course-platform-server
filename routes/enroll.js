78a40c77946faf6af8fd7e9dc8c2c5531a00b97a
George Tesha <teshageorge001@gmail.com>
2026-06-23 19:48:08 +0300

    Update enroll.js

--- routes/enroll.js
+++ routes/enroll.js
@@ -2,13 +2,19 @@ const router = require('express').Router();
 const {
   createOrder,
   verifyPayment,
-  getMyEnrollments
+  getMyEnrollments,
+  completeLesson,
+  getEnrollmentProgress
 } = require('../controllers/enrollController');
 const { protect } = require('../middleware/authMiddleware');
 
 // All enrollment routes require login
-router.post('/create-order',    protect, createOrder);
-router.post('/verify-payment',  protect, verifyPayment);
-router.get('/my-enrollments',   protect, getMyEnrollments);
+router.post('/create-order', protect, createOrder);
+router.post('/verify-payment', protect, verifyPayment);
+router.get('/my-enrollments', protect, getMyEnrollments);
+
+// NEW: Progress tracking routes
+router.post('/:enrollmentId/complete-lesson', protect, completeLesson);
+router.get('/:enrollmentId', protect, getEnrollmentProgress);
 
 module.exports = router;
